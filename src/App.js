8a2ba62aa8bd11e817d767175fce7f4b96ae1d59
Samson Aderonmu <94833173+Superior212@users.noreply.github.com>
2023-02-11 16:15:18 +0100

    updated version

--- src/App.js
+++ src/App.js
@@ -1,70 +1,80 @@
-import logo from "./logo.svg";
+import arrow from "./Assest/Arrow.png";
+import arrowUp from "./Assest/ArrowUp.png";
 import "./App.css";
 import { useState } from "react";
+import {FaEdit} from "react-icons/fa";
+import {FaTrash} from "react-icons/fa";
 
 function App() {
   const [todoInput, settodoInput] = useState("");
   const [allTodo, setallTodo] = useState([]);
-  const [editedIndex, seteditedIndex] = useState(null)
+  const [editedIndex, seteditedIndex] = useState(null);
+  
 
   function addTodoList() {
-  
-    if(editedIndex==null){
+    if (editedIndex == null && todoInput!=="") {
       let newTodo = [todoInput];
       setallTodo([...allTodo, newTodo]);
-     
-    }else{
-      let alleditedIndex =[...allTodo]
-      alleditedIndex[editedIndex]=todoInput
-      setallTodo(alleditedIndex)
+    } else {
+      let alleditedIndex = [...allTodo];
+      alleditedIndex[editedIndex] = todoInput;
+      setallTodo(alleditedIndex);
+      seteditedIndex(null);
     }
     settodoInput("");
   }
 
   function deleteTodo(deleteIndex) {
     console.log(deleteIndex);
-    let newAlltodo = [...allTodo]
-    let found = newAlltodo.filter((todo,index)=>(index!=deleteIndex))
-    setallTodo(found)
+    let newAlltodo = [...allTodo];
+    let found = newAlltodo.filter((todo, index) => index != deleteIndex);
+    setallTodo(found);
   }
 
-  function editTodo(editIndex){
-    seteditedIndex(editIndex)
-    settodoInput(allTodo[editIndex])
+  function editTodo(editIndex) {
+    seteditedIndex(editIndex);
+    settodoInput(allTodo[editIndex]);
+  }
+
+  function clearAllTodo() {
+    setallTodo([]);
   }
   return (
     <>
-      <input
-        type="text"
-        value={todoInput}
-        onChange={(e) => settodoInput(e.target.value)}
-        className="todo"
-      />
+      <h1>TODO</h1>
+      <div className="inputArea">
+        <div className="todoForm">
+          <input
+            type="text"
+            value={todoInput}
+            onChange={(e) => settodoInput(e.target.value)}
+            placeholder="Create a new todo..."
+          />
+          <button onClick={addTodoList} className="addbtn">
+            {" "}
+            {editedIndex == null ? (
+              <img src={arrow} className="imgArrow" />
+            ) : (
+              <img src={arrowUp} className="imgArrow" />
+            )}{" "}
+          </button>
+        </div>
+      </div>
 
-      <button onClick={addTodoList}> {editedIndex == null ? "Add Todo" : "Update"}</button>
-
-      <h1>Todo List</h1>
       {allTodo.map((todo, index) => (
-        <table key={index}>
-          <thead>
-            <tr>
-              <th>S/N</th>
-              <th>Todo</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{index + 1}</td>
-              <td>{todo}</td>
-              <td>
-                <button onClick={()=>editTodo(index)}>Edit</button>
-                <button onClick={() => deleteTodo(index)}>Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div key={index} className="displayArea">
+          <p className="textArea">{todo}</p>
+          <p className="BtnArea">
+            <button className="editBtn" onClick={() => editTodo(index)}><FaEdit/></button>
+            <button  className="displayBtn" onClick={() => deleteTodo(index)}><FaTrash/> </button>
+          </p>
+        </div>
       ))}
+    
+  <p className="CountArea"><span className="countText">{allTodo.length} Items</span>
+  
+<button onClick={clearAllTodo} className="clearBtn">Clear Complete</button>
+  </p>  
     </>
   );
 }
